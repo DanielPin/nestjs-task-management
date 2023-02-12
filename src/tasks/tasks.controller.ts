5ec980a57350912a4d5030de01664b4793d22d37
Daniel Nascimento <daniel.pin@outlook.com>
2023-02-12 20:00:15 -0300

    adicionando validacao

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,6 +12,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-update.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -44,8 +45,8 @@ export class TasksController {
   @Put('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
   ): Task {
-    return this.tasksService.updateTask(id, status);
+    return this.tasksService.updateTask(id, updateTaskStatusDto.status);
   }
 }
